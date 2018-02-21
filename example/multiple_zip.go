3f4d298b862b98e81c626bc2f255eb75a26b94c8
Jimut Dhali <jemdhali67@gmail.com>
2018-02-21 18:45:52 +0530

    updated examples with new api

--- example/multiple_zip.go
+++ example/multiple_zip.go
@@ -3,50 +3,38 @@
 package main
 
 import (
-    "io/ioutil"
-    ".." // import the sdk
-    "fmt"
+	"fmt"
+
+	"github.com/fusioncharts/fusionexport-go-client"
 )
 
-func saveFiles(fileBag []FusionExport.OutFileBag) {
-    for _, file := range fileBag {
-        fmt.Println(file.RealName)
-        fileData, err := ioutil.ReadFile(file.TmpPath)
-        check(err)
-        err = ioutil.WriteFile(file.RealName, fileData, 0644)
-        check(err)
-    }
-}
-
 // Called when export is done
-func onDone (outFileBag []FusionExport.OutFileBag, err error) {
-    check(err)
-    saveFiles(outFileBag)
+func onDone(outFileBag []FusionExport.OutFileBag, err error) {
+	check(err)
+	FusionExport.SaveExportedFiles(outFileBag)
 }
 
 // Called on each export state change
-func onStateChange (event FusionExport.ExportEvent) {
-    fmt.Println("[" + event.Reporter + "] " + event.CustomMsg)
+func onStateChange(event FusionExport.ExportEvent) {
+	fmt.Println("[" + event.Reporter + "] " + event.CustomMsg)
 }
 
 func main() {
-    // Instantiate ExportConfig and add the required configurations
-    exportConfig := FusionExport.NewExportConfig()
+	// Instantiate ExportConfig and add the required configurations
+	exportConfig := FusionExport.NewExportConfig()
 
-    chartConfig, err := ioutil.ReadFile("resources/multiple.json")
-    check(err)
-    exportConfig.Set("chartConfig", string(chartConfig))
-    exportConfig.Set("exportFile", "go-export-<%= number(5) %>")
-    exportConfig.Set("exportAsZip", "true")
+	exportConfig.Set("chartConfig", "example/resources/multiple.json")
+	exportConfig.Set("exportFile", "go-export-<%= number(5) %>")
+	exportConfig.Set("exportAsZip", true)
 
-    // Instantiate ExportManager
-    exportManager := FusionExport.NewExportManager()
-    // Call the Export() method with the export config and the respective callbacks
-    exportManager.Export(exportConfig, onDone, onStateChange)
+	// Instantiate ExportManager
+	exportManager := FusionExport.NewExportManager()
+	// Call the Export() method with the export config and the respective callbacks
+	exportManager.Export(exportConfig, onDone, onStateChange)
 }
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
--- example/svg_export.go
+++ example/svg_export.go
@@ -3,49 +3,36 @@
 package main
 
 import (
-    "io/ioutil"
-    ".." // import the sdk
-    "path/filepath"
-    "fmt"
+	"fmt"
+
+	"github.com/fusioncharts/fusionexport-go-client"
 )
 
-func saveFiles(fileBag []FusionExport.OutFileBag) {
-    for _, file := range fileBag {
-        fmt.Println(file.RealName)
-        fileData, err := ioutil.ReadFile(file.TmpPath)
-        check(err)
-        err = ioutil.WriteFile(file.RealName, fileData, 0644)
-        check(err)
-    }
-}
-
 // Called when export is done
-func onDone (outFileBag []FusionExport.OutFileBag, err error) {
-    check(err)
-    saveFiles(outFileBag)
+func onDone(outFileBag []FusionExport.OutFileBag, err error) {
+	check(err)
+	FusionExport.SaveExportedFiles(outFileBag)
 }
 
 // Called on each export state change
-func onStateChange (event FusionExport.ExportEvent) {
-    fmt.Println("[" + event.Reporter + "] " + event.CustomMsg)
+func onStateChange(event FusionExport.ExportEvent) {
+	fmt.Println("[" + event.Reporter + "] " + event.CustomMsg)
 }
 
 func main() {
-    // Instantiate ExportConfig and add the required configurations
-    exportConfig := FusionExport.NewExportConfig()
+	// Instantiate ExportConfig and add the required configurations
+	exportConfig := FusionExport.NewExportConfig()
 
-    svg, err := filepath.Abs("resources/vector.svg")
-    check(err)
-    exportConfig.Set("inputSVG", svg)
+	exportConfig.Set("inputSVG", "example/resources/vector.svg")
 
-    // Instantiate ExportManager
-    exportManager := FusionExport.NewExportManager()
-    // Call the Export() method with the export config and the respective callbacks
-    exportManager.Export(exportConfig, onDone, onStateChange)
+	// Instantiate ExportManager
+	exportManager := FusionExport.NewExportManager()
+	// Call the Export() method with the export config and the respective callbacks
+	exportManager.Export(exportConfig, onDone, onStateChange)
 }
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
