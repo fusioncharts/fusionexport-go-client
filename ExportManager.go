acc063fbfc0703a2ff10f4959fcdfce9a888e375
Poushali Guha <poushali@collabion.com>
2018-06-25 11:56:48 +0530

    amazons3 file upload support has been added export example for different libraries has been added

--- ExportManager.go
+++ ExportManager.go
@@ -1,10 +1,20 @@
 package FusionExport
 
 import (
+	"fmt"
 	"encoding/base64"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"context" // needed for s3
+	"bytes" // needed for s3 and ftp
+	"net/http" // needed for s3
+	"github.com/aws/aws-sdk-go/aws"  // needed for s3
+	"github.com/aws/aws-sdk-go/aws/credentials"  // needed for s3
+	"github.com/aws/aws-sdk-go/service/s3" // needed for s3
+	"github.com/aws/aws-sdk-go/aws/session" // needed for s3
+	"github.com/aws/aws-sdk-go/service/s3/s3manager" // needed for s3
+	
 )
 
 type ExportManager struct {
@@ -60,6 +70,53 @@ func SaveExportedFiles(fileBag []OutFileBag) error {
 
 	return nil
 }
+func GetRegion(bucket string) string {
+	sess := session.Must(session.NewSession())
+	region, err := s3manager.GetBucketRegion(context.Background(), sess, bucket, "us-west-2")
+	if err != nil {
+	   return err.Error();
+	}
+	return region
+}
+func UploadFileToAmazonS3(fileBag []OutFileBag, bucket string, accessKey string, secretAccessKey string){
+	aws_access_key_id := accessKey
+	aws_secret_access_key := secretAccessKey
+	token := "" 
+	creds := credentials.NewStaticCredentials(aws_access_key_id, aws_secret_access_key, token) 
+	_, err := creds.Get() 
+	if err != nil { 
+		fmt.Printf("invalid credentials: %s", err) 
+	} 
+	region := GetRegion(bucket)
+	cfg := aws.NewConfig().WithRegion(region).WithCredentials(creds) 
+	svc := s3.New(session.New(), cfg) 
+	for _, file := range fileBag {
+		fileData, err := base64.StdEncoding.DecodeString(file.FileContent)
+		if err != nil {
+			fmt.Printf("%s",err)
+			
+		}
+		size := int64(len(fileData))
+
+		fileBytes := bytes.NewReader(fileData) 
+		fileType := http.DetectContentType(fileData) 
+		params := &s3.PutObjectInput{ 
+		Bucket: aws.String(bucket), 
+		Key: aws.String(file.RealName), 
+		Body: fileBytes, 
+		ContentLength: aws.Int64(size), 
+		ContentType: aws.String(fileType), 
+		} 
+		resp, err := svc.PutObject(params) 
+		if err != nil { 
+			fmt.Printf("bad response;response: %s, error: %s", resp,err) 
+		} 
+		fmt.Printf("file uploaded")
+
+		
+	} 
+}
+
 
 func GetExportedFileNames(fileBag []OutFileBag) []string {
 	var fns []string
---
+++ example/highcharts_exp.go
@@ -0,0 +1,40 @@
+// Highcharts export
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/fusioncharts/fusionexport-go-client"
+)
+
+// Called when export is done
+func onDone(outFileBag []FusionExport.OutFileBag, err error) {
+	check(err)
+	FusionExport.SaveExportedFiles(outFileBag)
+}
+
+// Called on each export state change
+func onStateChange(event FusionExport.ExportEvent) {
+	fmt.Println("[" + event.Reporter + "] " + event.CustomMsg)
+}
+
+func main() {
+	// Instantiate ExportConfig and add the required configurations
+	exportConfig := FusionExport.NewExportConfig()
+
+	exportConfig.Set("templateFilePath", "example/resources/template_highcharts.html")
+	exportConfig.Set("type", "pdf")
+	exportConfig.Set("asyncCapture", true)
+
+	// Instantiate ExportManager
+	exportManager := FusionExport.NewExportManager()
+	// Call the Export() method with the export config and the respective callbacks
+	exportManager.Export(exportConfig, onDone, onStateChange)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
